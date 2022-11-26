e264a6cd15bf3521319f69f99f72b1d1569b1bba
jerry <jerrynwosu007@gmail.com>
2022-11-26 14:28:46 +0100

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `GeekReflex`,
+    author: `Jerry Nwosu`,
+    description: `GeekReflex Personal Blog`,
+    siteUrl: `https://geekreflex.netlify.app/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `geekreflex`,
     },
   },
   plugins: [
