7d18be4d9f6282e400fbd1a864e869457fe8865b
jerry <jerrynwosu007@gmail.com>
2022-12-10 02:21:26 +0100

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,13 +2,7 @@ import React from "react"
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
-import { Provider } from "react-redux"
-import { store } from "./src/app/store"
 
 export const wrapRootElement = ({ element }) => {
-  return (
-    <React.Fragment>
-      <Provider store={store}>{element}</Provider>
-    </React.Fragment>
-  )
+  return <React.Fragment>{element}</React.Fragment>
 }
