362361dfc50a4fd5ce57ec4a56a22fb0b2ffbd31
jerry <jerrynwosu007@gmail.com>
2022-12-10 01:40:30 +0100

    update query

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,8 +81,8 @@ module.exports = {
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.edges.map((edge) => {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map((edge) => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
                   date: edge.node.frontmatter.date,
